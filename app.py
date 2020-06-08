2c3f650d8bbaaf2b3849ddcaa1ec3b21ab7855ee
Kim-Hong-Jae <50355686+Kim-Hong-Jae@users.noreply.github.com>
2020-06-08 15:12:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,33 @@ def index():
     return render_template('index.html',user = user, action = "")
 
 
+# 네이버 북 api 요청
+def search_book(query):
+    CLIENT_ID = "UQDh6UhuGYrRhhQvg3eI"
+    CLIENT_SECRET = "UVeMYwBp9v"
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode, quote
+    import json
+    request = Request('https://openapi.naver.com/v1/search/book?query=' + quote(query))
+    request.add_header('X-Naver-Client-Id', CLIENT_ID)
+    request.add_header('X-Naver-Client-Secret', CLIENT_SECRET)
+    response = urlopen(request).read().decode('utf-8')
+    search_result = json.loads(response)
+    return search_result
+
+#search 페이지 - 임시
+@app.route('/search', methods = ['GET','POST'])
+def search():
+    if request.method == 'POST':
+        search_name = request.form['input']
+    else:
+        search_name = request.args.get('input')
+    book_name = search_name
+    search_result = search_book('%s' % book_name)['items']
+    return render_template('search.html', search_result=search_result)
+
+
+
 @app.route('/login', methods = ['GET','POST'])
 def login():
     print("Login action")
